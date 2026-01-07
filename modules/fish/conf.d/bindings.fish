9d835f867bbb68f8ce15ee2c77e1f3b200d7359b
VasKho <vasya.khoroshavin@gmail.com>
2026-01-07 19:39:38 +0300

    Update fish config for latest version

--- modules/fish/conf.d/bindings.fish
+++ modules/fish/conf.d/bindings.fish
@@ -1,59 +1,58 @@
 function fish_custom_key_bindings -d "Custom keybindings"
     function kill_char_or_region
-	if test -z (commandline -s)
-	    commandline -f backward-delete-char
-	else
-	    commandline -f kill-selection
-	    commandline -f end-selection
-	end
+				if test -z (commandline -s)
+						commandline -f backward-delete-char
+				else
+						commandline -f kill-selection
+						commandline -f end-selection
+				end
     end
 
-    bind \cB 'backward-char'
-    bind \e\[D 'backward-char'
-    bind \cF 'forward-char'
-    bind \e\[C 'forward-char'
+    bind ctrl-b 'backward-char'
+    bind left 'backward-char'
+    bind ctrl-f 'forward-char'
+    bind right 'forward-char'
 
-    bind \eb 'backward-word'
-    bind \e\[1\;3D 'backward-word'
-    bind \e\[1\;5D 'backward-word'
-    bind \ef 'forward-word'
-    bind \e\[1\;3C 'forward-word'
-    bind \e\[1\;5C 'forward-word'
+    bind ctrl-left 'backward-word'
+    bind alt-left 'backward-word'
+    bind alt-b 'backward-word'
+    bind ctrl-right 'forward-word'
+    bind alt-left 'forward-word'
+    bind alt-f 'forward-word'
 
-    bind \x7F 'kill_char_or_region'
-    bind \cD 'delete-char'
+    bind backspace 'kill_char_or_region'
+    bind ctrl-d 'delete-char'
 
-    bind \e\x7F 'backward-kill-word'
-    bind \e\x7F 'backward-kill-path-component'
-    bind \ed 'kill-word'
-    bind \cK 'kill-line'
+    bind alt-backspace 'backward-kill-word'
+    bind alt-backspace 'backward-kill-path-component'
+    bind alt-d 'kill-word'
+    bind ctrl-k 'kill-line'
 
-    bind \cA 'beginning-of-line'
-    bind \cE 'end-of-line'
+    bind ctrl-a 'beginning-of-line'
+    bind ctrl-e 'end-of-line'
 
-    bind \cX\cU 'upcase-word'
-    bind \cX\cL 'downcase-word'
+    bind ctrl-x,ctrl-u 'upcase-word'
+    bind ctrl-x,ctrl-l 'downcase-word'
 
-    # bind \cR 'history-pager'
-    # bind \cX\cD 'history-pager-delete'
-    bind \cS 'pager-toggle-search'
-    bind \e\[A 'up-or-search'
-    bind \cP 'up-or-search'
-    bind \e\[B 'down-or-search'
-    bind \cN 'down-or-search'
-    bind \c_ 'undo'
-    bind \e/ 'redo'
+    bind ctrl-x,ctrl-d 'history-delete'
+    bind ctrl-s 'pager-toggle-search'
+    bind up 'up-or-search'
+    bind ctrl-p 'up-or-search'
+    bind down 'down-or-search'
+    bind ctrl-n 'down-or-search'
+    bind ctrl-/ 'undo'
+    bind alt-/ 'redo'
 
-    bind -k nul 'begin-selection'
-    bind \cG 'end-selection' 'cancel'
-    bind \cX\cX 'swap-selection-start-stop'
+    bind ctrl-space 'begin-selection'
+    bind ctrl-g 'end-selection' 'cancel'
+    bind ctrl-x,ctrl-x 'swap-selection-start-stop'
 
-    bind \cY 'fish_clipboard_paste'
-    bind \ew 'fish_clipboard_copy'
-    bind \cX\cE 'edit_command_buffer'
-    bind \t 'complete'
+    bind ctrl-y 'fish_clipboard_paste'
+    bind alt-w 'fish_clipboard_copy'
+    bind ctrl-x,ctrl-e 'edit_command_buffer'
+    bind tab 'complete'
 
-    bind \cX\cP 'toggle_private_mode; commandline -f repaint'
+    bind ctrl-x,ctrl-p 'toggle_private_mode; commandline -f repaint'
 end
 
 set -g fish_key_bindings fish_custom_key_bindings
--- modules/fish/config.fish
+++ modules/fish/config.fish
@@ -1,23 +1,31 @@
 if status is-interactive
     if ! functions -q fisher
-	curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && \
-	    fisher install jorgebucaran/fisher
+				curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && \
+						fisher install jorgebucaran/fisher
 
-	fisher install PatrickF1/fzf.fish
+				fisher install PatrickF1/fzf.fish
     end
 
+		set -x XDG_CONFIG_HOME $HOME/.config
+		set -x TERM xterm-256color
     set -x MOZ_ENABLE_WAYLAND 1
     set -x DESKTOP_APP_I_KNOW_ABOUT_GTK_INCOMPATIBILITY 1
     set -x PAGER less
     set -x MANPAGER less
-    set -x EDITOR nvim
+    set -x EDITOR emacs -nw
     set -x DOCKER_BUILDKIT 1
+		set -x ANDROID_HOME /opt/android-sdk
+		set -x GOOGLE_CLOUD_PROJECT_ID ""
+    set -x SUDO_EDITOR "emacs -nw --file"
+    set -x TERMCMD "foot"
 
-    set -x PATH "$PATH:/home/$USER/.cargo/bin:/home/$USER/texlive/bin/x86_64-linux:/home/$USER/.local/bin:/home/$USER/.roswell/bin"
+		fish_add_path -a -P "$HOME/.cargo/bin"
+		fish_add_path -a -P "$HOME/texlive/bin/x86_64-linux"
+		fish_add_path -a -P "$HOME/.local/bin"
 end
 
 function em -d 'Alias for terminal emacs client'
-    emacsclient -nw $argv
+    emacs -nw --file $argv
 end
 
 function dbx -d 'Alias for distrobox'
