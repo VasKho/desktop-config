bf39f397fe779c17bcf577f79edd563eac940350
VasKho <vasya.khoroshavin@gmail.com>
2026-02-08 12:04:48 +0300

    Fix condition in insert keybinding

--- modules/home/karabiner/config/emacs.nix
+++ modules/home/karabiner/config/emacs.nix
@@ -152,10 +152,7 @@ in {
           (gen.clearVar "control-spacebar")
           (gen.runSh "pkill key_overlay")
         ];
-        conditions = [
-          (gen.ifVarSet "control-spacebar")
-          (gen.unlessApps excludeApps)
-        ];
+        conditions = [ (gen.unlessApps excludeApps) ];
       }
 
       {
