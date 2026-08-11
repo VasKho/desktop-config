06f78c230783c2cfd4d78012fb2f53ef5e23a9b6
VasKho <vasya.khoroshavin@gmail.com>
2026-08-11 14:20:09 +0300

    feat: unified fish keybindings

--- modules/home/fish/config.fish
+++ modules/home/fish/config.fish
@@ -1,13 +1,13 @@
 if status is-interactive
 	set -x TERM xterm-256color
-    set -x DOCKER_BUILDKIT 1
+  set -x DOCKER_BUILDKIT 1
 end
 
 function dbx -d 'Alias for distrobox'
-    set -lx TERM xterm-256color
-    distrobox $argv
+  set -lx TERM xterm-256color
+  distrobox $argv
 end
 
 function ll -d 'Alias for ls'
-    ls --color -lAh $argv
+  ls --color -lAh $argv
 end
